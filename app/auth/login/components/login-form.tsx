2dad6658c186725cd8da2c58a9b90da2b6d2858f
Gerald-ux-ux <11geraldmoney@gmail.com>
2025-05-09 17:32:50 +0300

    feat: installing supabase

--- app/auth/login/components/login-form.tsx
+++ app/auth/login/components/login-form.tsx
@@ -23,7 +23,7 @@ export default function LoginForm() {
         <div className="flex items-center justify-between">
           <Label htmlFor="password">Password</Label>
           <Link
-            href="/auth/forgot-password"
+            href="/auth/login"
             className="text-sm font-medium text-primary hover:underline"
           >
             Forgot password?
